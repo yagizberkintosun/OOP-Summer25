63a5b47a869188913a8ca2c7f9516f10d18c89f7
yagizberkintosun <berkinyagiz35@gmail.com>
2025-03-06 15:04:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-# change it to print your name
+class Person:
+  def __init__(self, name, age,address):
+    self.name = name
+    self.age = age
+    self.address = address
 
-print("Hello, I am Piotr Brudny")
+p1 = Person("Yagiz Berkin", 21 ,'Kopinska 12/16 02-321 Warszawa Polska')
+
+print(p1.name)
+print(p1.age)
+print(p1.address)
