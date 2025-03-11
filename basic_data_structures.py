d908eff9fa8eb53943ad59d9ecb5c90d2c90baae
yagizberkintosun <berkinyagiz35@gmail.com>
2025-03-11 11:22:44 +0100

    Create basic_data_structures.py

---
+++ basic_data_structures.py
@@ -0,0 +1,8 @@
+my_age = 21 # integer
+my_name = yagiz # string
+my_height = 1.78 #float
+my_list = [21,'yagiz',1.78] # lists
+my_car = {'brand': 'ford' 'model': 'mustang' 'year': 1964' #dictionary 
+my_tuples = ('red' , 'white') #tuple
+my_unique_numbers = {1,2,3,4} #set
+is_student = True # true or false
