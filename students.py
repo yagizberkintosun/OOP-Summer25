ad8346d579e52622b1adc4ef04d73e10069993fb
yagizberkintosun <berkinyagiz35@gmail.com>
2025-03-13 14:25:55 +0100

    students.py

---
+++ students.py
@@ -0,0 +1,9 @@
+students = [
+    {'index': 35560, 'name': 'Yagiz Berkin Tosun'},
+    {'index': 21350, 'name': 'Lionel Messi'},
+    {'index': 35021, 'name': 'Robert Lewandowski'}
+]
+
+for student in students:
+    print(f"Index: {student['index']}, Name: {student['name']}")
+  
