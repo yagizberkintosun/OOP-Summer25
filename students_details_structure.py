f0832b57eedbffa6ce503c846fdb483ea6d81281
yagizberkintosun <berkinyagiz35@gmail.com>
2025-03-13 14:48:46 +0100

    students_details_structure.py

--- students_details_structure.py
+++ students_details_structure.py
@@ -1,10 +1,18 @@
 student = {
-    'first_name': 'Yagiz',
-    'second_name': 'Berkin',
-    'last_name': 'Tosun'
+    'first name': 'Yagiz',
+    'second name': 'Berkin',
+    'last name': 'Tosun',
+    'index number': '35560',
+    'nationality': 'Turkish',
+    'starting date': '13/10/2025',
+    'my courses': 'Object Oriented Programming'
 }
 
-print(student['first_name'])
-print(student['second_name'])
-print(student['last_name'])
+print(student['first name'])
+print(student['second name'])
+print(student['last name'])
+print(student['index number'])
+print(student['nationality'])
+print(student['starting date'])
+print(student['my courses'])
 
