64b68978f17c1c58db408c93643b44eaa03f34d7
yagizberkintosun <berkinyagiz35@gmail.com>
2025-03-13 13:59:40 +0100

    students_details_structure.py

--- students_details_structure.py
+++ students_details_structure.py
@@ -1,5 +1,8 @@
-student1 = {
+student = {
  'first_name':'Yagiz'
   'second_name':'Berkin  
    'last_name':'Tosun'
 }
+print(student.first_name)
+print(student.second-name)
+print(student.last_name)
