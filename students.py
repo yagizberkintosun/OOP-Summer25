53399d103e492bfd3c113d612441460eb292af95
yagizberkintosun <berkinyagiz35@gmail.com>
2025-03-13 15:11:55 +0100

    students.py

--- students.py
+++ students.py
@@ -1,9 +1,47 @@
 students = [
-    {'index': 35560, 'name': 'Yagiz Berkin Tosun'},
-    {'index': 21350, 'name': 'Lionel Messi'},
-    {'index': 35021, 'name': 'Robert Lewandowski'}
+   {
+      "first_name": "Yagiz",
+       "second_name": "Berkin"
+      "last_name": "Tosun",
+      "index_number": "35522",
+      "nationality": "Turkish",
+      "starting_date": "2025-03-01",
+      "courses" : ["Object Oriented Programming","Basics of Telecomunication","Mathematics"]
+  },{
+      "first_name": "Berk",
+      "last_name": "Saritas",
+      "index_number": "35236",
+      "nationality": "Turkish",
+      "starting_date": "2025-03-01",
+      "courses" : ["Object Oriented Programming","Basics of Telecomunication","Mathematics"]
+  },{
+      "first_name": "Serhat",
+      "last_name": "Kalkan",
+      "index_number": "35522",
+      "nationality": "Turkish",
+      "starting_date": "2025-03-01",
+      "courses" : ["Object Oriented Programming","Basics of Telecomunication","Mathematics"]
+  }
 ]
-
 for student in students:
-    print(f"Index: {student['index']}, Name: {student['name']}")
+         print(f"Name:
+         {student['first_name']}
+         {student['last_name']}, Index:
+         {student['index_number']}")
+
+React
+
+Reply
+
+
+
+
+
+
+
+
+
+
+
+        
   
