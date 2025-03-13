b42887703900cd499a97b506f0fa0f23ff1ff2f6
yagizberkintosun <berkinyagiz35@gmail.com>
2025-03-13 14:59:01 +0100

    student_details.py

--- student_details.py
+++ student_details.py
@@ -1,10 +1,10 @@
-first_name = "Yagiz"
-second_name = "Berkin"
-last_name = "Tosun"
-index_number = "35560"
-my_nationality = "Turkish"
-starting_date = "13/10/2024"
-my_courses = "Object Oriented Programming"
+first_name = ["Yagiz"]
+second_name = ["Berkin"]
+last_name = ["Tosun"]
+index_number = ["35560"]
+my_nationality = ["Turkish"]
+starting_date = ["13/10/2024"]
+my_courses = ["Object Oriented Programming"]
 
 print("First Name:", first_name)
 print("Second Name:", second_name)
